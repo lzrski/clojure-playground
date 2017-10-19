c4c9c713de614a029350199ade20e2c3ff082622
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-10-19 12:52:04 +0000

    Refactor math exercise: un-in-line expressions and iterator
    Now it's both DRY and readable. Like.
    
    Also I've experimented a little with formatting. Lisp is a new element
    for me. There is this style guide for Clojure
    https://github.com/bbatsov/clojure-style-guide to be read later.

--- math.clj
+++ math.clj
@@ -1,6 +1,17 @@
-(prn (map
-  #(str % " -> " (eval %))
-  [ '(Math/cos Math/PI)
-    '(let [x (Math/random) ]
-      (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2 ))
-    )]))
+(def
+  expressions
+  [
+    '(Math/cos Math/PI)
+    '(let
+      [x (Math/random)]
+      (+
+        (Math/pow (Math/sin x) 2)
+        (Math/pow (Math/cos x) 2)))
+  ])
+
+(def
+  iterator
+  #(str % " -> " (eval %)))
+
+(prn
+  (map iterator expressions))
