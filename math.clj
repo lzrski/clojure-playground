5c9416a8fc97a23b4b1948d01fc85b13aad71a95
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-10-19 12:30:41 +0000

    Refactor math exercise to use map
    Vector of math expressions is mapped to a function that concatenates
    them with their value. The result is printed.
    
    Note that map is lazy, so printing must be outside of the iterator
    function. Otherwis nothing is going to happen.
    
    I'm not sure if this is more readable. It's defienitely more concise.

--- math.clj
+++ math.clj
@@ -1,11 +1,6 @@
-(def
-  expr '(Math/cos Math/PI))
-(println expr " -> " (eval expr))
-
-(def expr
-  '(let [x (Math/random)]
-    (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2))
-  )
-)
-
-(println expr " -> " (eval expr))
+(prn (map
+  #(str % " -> " (eval %))
+  [ '(Math/cos Math/PI)
+    '(let [x (Math/random) ]
+      (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2 ))
+    )]))
