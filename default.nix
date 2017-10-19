0a8a1866aad28236f73ab35a9c624e63c7c90820
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-10-19 08:29:32 +0000

    Add leiningen and boot to environment
    They are build / automation tools. I think they are alternatives (you
    use one or the other), but there is a room for everyone in the
    playground.
    
    To get REPL with readline support use
    
        lein repl

--- default.nix
+++ default.nix
@@ -2,6 +2,8 @@ with import <nixpkgs> {}; stdenv.mkDerivation {
   name = "Clojure";
   buildInputs = [
     clojure
+    leiningen
+    boot
   ];
   shellHook = ''
     # Start user's preffered shell
